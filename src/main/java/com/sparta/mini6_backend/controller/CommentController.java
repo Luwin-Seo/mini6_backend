5c2f176cc42273bb8b6499f1f2597a241eb44b4b
Son-Gyeongi <thsruddl77@gmail.com>
2022-06-10 22:11:01 +0900

    tempSave : 커밋, 푸시 점검

--- src/main/java/com/sparta/mini6_backend/controller/CommentController.java
+++ src/main/java/com/sparta/mini6_backend/controller/CommentController.java
@@ -1,4 +1,17 @@
 package com.sparta.mini6_backend.controller;
 
+import com.sparta.mini6_backend.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    //댓글 조회
+    @GetMapping("/api/comment/{articleId}")
+    public
 }
