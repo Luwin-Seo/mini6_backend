71db32c40e9442650b2d24c77f25baa6f01ff27c
gaius365 <gaius365@gmail.com>
2022-06-15 18:01:10 +0900

    edit: LikeController URL 수정

--- src/main/java/com/sparta/mini6_backend/controller/LikeController.java
+++ src/main/java/com/sparta/mini6_backend/controller/LikeController.java
@@ -7,10 +7,7 @@ import com.sparta.mini6_backend.security.UserDetailsImpl;
 import com.sparta.mini6_backend.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,14 +17,14 @@ public class LikeController {
 
     private final LikeRepository likeRepository;
 
-    @PutMapping("/article/{articleId}/like")
+    @PutMapping("/api/articles/{articleId}/like")
     public int updateLike (@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long articleId) {
         if (userDetails == null){throw new CustomException(ErrorCode.AUTH_TOKEN_NOT_FOUND);}
         likeService.updateLike(articleId,userDetails.getUser().getUserId());
         return likeRepository.findAllByArticleId(articleId).size();
     }
 
-    @GetMapping("/article/{articleId}/like")
+    @GetMapping("/api/articles/{articleId}/like")
     public int getLike(@PathVariable Long articleId) {
         return likeRepository.findAllByArticleId(articleId).size();
     }
