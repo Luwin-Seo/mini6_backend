1b4af913b92397f1773570a8951e620c464b6fba
Luwin-Seo <aksdnjs88@gmail.com>
2022-06-16 10:43:31 +0900

    feat : comment 정렬순서 역전

--- src/main/java/com/sparta/mini6_backend/repository/CommentRepository.java
+++ src/main/java/com/sparta/mini6_backend/repository/CommentRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findByArticleIdOrderByCreatedAtDesc(Long articleId);
+    List<Comment> findByArticleIdOrderByCreatedAtAsc(Long articleId);
     //findByArticleId By OrderBy CreatedAtDesc 수정시간된 날짜를 기준으로 내림차순으로 정렬해줘(최신순으로 정렬해줘)
     //최신순으로 정렬하는 SQL문을 JPA가 다 알아서 짜준다.
 
