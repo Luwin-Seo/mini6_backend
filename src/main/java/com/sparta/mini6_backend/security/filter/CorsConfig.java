afa7f5b25a1320ca6788f437aa341f8cfa75fb76
gaius365 <gaius365@gmail.com>
2022-06-16 17:53:51 +0900

    edit: CORS 로컬호스트도 동시에 허용

--- src/main/java/com/sparta/mini6_backend/security/filter/CorsConfig.java
+++ src/main/java/com/sparta/mini6_backend/security/filter/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {//크로스 오리진 정책 설정
         CorsConfiguration config=new CorsConfiguration();
         config.setAllowCredentials(true);// 내 서버가 응답을 할 때 Json을 자바스크립트에서 처리할 수 있게 할지를 결정하는 것
         config.addAllowedOrigin("http://devbox.com.s3-website.ap-northeast-2.amazonaws.com/");//모든 ip에 응답을 허용
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");//모든 헤더에 응답 허용
         config.addAllowedMethod("*");//모든 http 메서드 응답 허용
         config.addExposedHeader("*");
